3ab05198609b35ecbec7ae7ded017d66701d7087
pieta-aeon <154050871+pieta-aeon@users.noreply.github.com>
2024-12-06 00:26:45 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Configuration template file
 // Copy this file to config.js and replace the API key with your own
 const config = {
-    API_KEY: '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',
+    API_KEY: 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,
     MAX_HISTORY_ITEMS: 5,
     ERRORS: {
         API_ERROR: "The spirits are temporarily unavailable. Try again later.",
